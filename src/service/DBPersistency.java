365438f862de36dcde603a8b73b963b331022a67
Martin <martin.petkov02@sap.com>
2023-11-28 12:24:38 +0200

    Refactor DBPersistency

--- src/service/DBPersistency.java
+++ src/service/DBPersistency.java
@@ -13,129 +13,136 @@ import model.Reader;
 
 public class DBPersistency implements Persistency {
 
+    private static final String BOOKS_TABLE = "Books";
+    private static final String READERS_TABLE = "Readers";
     private Connection connection;
 
     public DBPersistency(String dbURL) throws SQLException {
-        String userName = System.getenv("DB_USERNAME");
-        String password = System.getenv("DB_PASSWORD");
-        this.connection = DriverManager.getConnection(dbURL, userName, password); 
+        this.connection = DriverManager.getConnection(dbURL, System.getenv("DB_USERNAME"), System.getenv("DB_PASSWORD"));
     }
 
     @Override
     public void saveData(List<?> list, String tableName) throws SQLException {
-        if (tableName.equals("Books")) {
-            String updateQuery = "UPDATE Books SET availableQuantity = ?, totalQuantity = ? WHERE title = ? AND author = ?";
-            String insertQuery = "INSERT IGNORE INTO Books (title, author, availableQuantity, totalQuantity) SELECT ?,?,?,? FROM dual WHERE NOT EXISTS (SELECT * FROM Books WHERE title = ? AND author = ?)";
-            for (Object obj : list) {
-                if (obj instanceof Book) {
-                    Book book = (Book) obj;
-                    
-                    // First try to update existing book
-                    PreparedStatement updatePs = connection.prepareStatement(updateQuery);
-                    updatePs.setInt(1, book.getAvailableQuantity());
-                    updatePs.setInt(2, book.getTotalQuantity());
-                    updatePs.setString(3, book.getTitle());
-                    updatePs.setString(4, book.getAuthor());
-                    int updated = updatePs.executeUpdate();
-
-                    // If book doesn't exist, insert new book
-                    if (updated == 0) {
-                        PreparedStatement insertPs = connection.prepareStatement(insertQuery);
-                        insertPs.setString(1, book.getTitle());
-                        insertPs.setString(2, book.getAuthor());
-                        insertPs.setInt(3, book.getAvailableQuantity());
-                        insertPs.setInt(4, book.getTotalQuantity());
-                        insertPs.setString(5, book.getTitle());
-                        insertPs.setString(6, book.getAuthor());
-                        insertPs.executeUpdate();
-                    }
-                }
-            }
-        } else if (tableName.equals("Readers")) {
-            String insertBorrowedBooksQuery = "INSERT INTO BorrowedBooks (name, title, author) SELECT ?,?,? FROM dual WHERE NOT EXISTS (SELECT * FROM BorrowedBooks WHERE name = ? AND title = ? AND author = ?)";
-            String updateReaderQuery = "INSERT INTO Readers (name) SELECT ? FROM dual WHERE NOT EXISTS (SELECT * FROM Readers WHERE name = ?)";
-            for (Object obj : list) {
-                if (obj instanceof Reader) {
-                    Reader reader = (Reader) obj;
-
-                    PreparedStatement updateReaderPs = connection.prepareStatement(updateReaderQuery);
-                    updateReaderPs.setString(1, reader.getName());
-                    updateReaderPs.setString(2, reader.getName());
-                    updateReaderPs.executeUpdate();
-
-                    for (Book borrowedBook : reader.getBorrowedBooks()) {
-                        PreparedStatement insertBorrowedBooksPs = connection.prepareStatement(insertBorrowedBooksQuery);
-                        insertBorrowedBooksPs.setString(1, reader.getName());
-                        insertBorrowedBooksPs.setString(2, borrowedBook.getTitle());
-                        insertBorrowedBooksPs.setString(3, borrowedBook.getAuthor());
-                        insertBorrowedBooksPs.setString(4, reader.getName());
-                        insertBorrowedBooksPs.setString(5, borrowedBook.getTitle());
-                        insertBorrowedBooksPs.setString(6, borrowedBook.getAuthor());
-                        insertBorrowedBooksPs.executeUpdate();
-                    }
-                }
-            }
+        if (tableName.equals(BOOKS_TABLE)) {
+            handleBooks(list);
+        } else if (tableName.equals(READERS_TABLE)) {
+            handleReaders(list);
         } else {
             throw new IllegalArgumentException("Unknown table: " + tableName);
         }
     }
 
+    private void handleBooks(List<?> list) throws SQLException {
+        String updateQuery = "UPDATE Books SET availableQuantity = ?, totalQuantity = ? WHERE title = ? AND author = ?";
+        String insertQuery = "INSERT IGNORE INTO Books (title, author, availableQuantity, totalQuantity) SELECT ?,?,?,? FROM dual WHERE NOT EXISTS (SELECT * FROM Books WHERE title = ? AND author = ?)";
+        PreparedStatement updatePs = connection.prepareStatement(updateQuery);
+        PreparedStatement insertPs = connection.prepareStatement(insertQuery);
+
+        for (Object obj : list) {
+            if (obj instanceof Book) {
+                Book book = (Book) obj;
+
+                updatePs.setInt(1, book.getAvailableQuantity());
+                updatePs.setInt(2, book.getTotalQuantity());
+                updatePs.setString(3, book.getTitle());
+                updatePs.setString(4, book.getAuthor());
+                int updated = updatePs.executeUpdate();
+
+                if (updated == 0) {
+                    insertPs.setString(1, book.getTitle());
+                    insertPs.setString(2, book.getAuthor());
+                    insertPs.setInt(3, book.getAvailableQuantity());
+                    insertPs.setInt(4, book.getTotalQuantity());
+                    insertPs.setString(5, book.getTitle());
+                    insertPs.setString(6, book.getAuthor());
+                    insertPs.executeUpdate();
+                }
+            }
+        }
+    }
+
+    private void handleReaders(List<?> list) throws SQLException {
+        String insertBorrowedBooksQuery = "INSERT INTO BorrowedBooks (name, title, author) SELECT ?,?,? FROM dual WHERE NOT EXISTS (SELECT * FROM BorrowedBooks WHERE name = ? AND title = ? AND author = ?)";
+        String updateReaderQuery = "INSERT INTO Readers (name) SELECT ? FROM dual WHERE NOT EXISTS (SELECT * FROM Readers WHERE name = ?)";
+        PreparedStatement updateReaderPs = connection.prepareStatement(updateReaderQuery);
+        PreparedStatement insertBorrowedBooksPs = connection.prepareStatement(insertBorrowedBooksQuery);
+
+        for (Object obj : list) {
+            if (obj instanceof Reader) {
+                Reader reader = (Reader) obj;
+
+                updateReaderPs.setString(1, reader.getName());
+                updateReaderPs.setString(2, reader.getName());
+                updateReaderPs.executeUpdate();
+
+                for (Book borrowedBook : reader.getBorrowedBooks()) {
+                    insertBorrowedBooksPs.setString(1, reader.getName());
+                    insertBorrowedBooksPs.setString(2, borrowedBook.getTitle());
+                    insertBorrowedBooksPs.setString(3, borrowedBook.getAuthor());
+                    insertBorrowedBooksPs.setString(4, reader.getName());
+                    insertBorrowedBooksPs.setString(5, borrowedBook.getTitle());
+                    insertBorrowedBooksPs.setString(6, borrowedBook.getAuthor());
+                    insertBorrowedBooksPs.executeUpdate();
+                }
+            }
+        }
+    }
+
     @Override
     public List<?> loadData(String tableName) throws SQLException {
-        ResultSet resultSet;
-        if (tableName.equalsIgnoreCase("Books")) {
-            resultSet = connection.createStatement().executeQuery("SELECT * FROM Books");
-            List<Book> books = new ArrayList<>();
-            while (resultSet.next()) {
-                String title = resultSet.getString("title");
-                String author = resultSet.getString("author");
-                int availableQuantity = resultSet.getInt("availableQuantity");
-                int totalQuantity = resultSet.getInt("totalQuantity");
-                books.add(new Book(title, author, availableQuantity, totalQuantity));
-            }
-            return books;
+        if (tableName.equalsIgnoreCase(BOOKS_TABLE)) {
+            return loadBooks();
+        } else if (tableName.equalsIgnoreCase(READERS_TABLE)) {
+           return loadReaders();
         }
-        
-        else if (tableName.equalsIgnoreCase("Readers")) {
-            resultSet = connection.createStatement().executeQuery("SELECT * FROM Readers");
-            List<Reader> readers = new ArrayList<>();
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-
-                PreparedStatement ps = connection.prepareStatement("SELECT title, author FROM BorrowedBooks WHERE name = ?");
-                ps.setString(1, name);
-                ResultSet borrowedBooksResultSet = ps.executeQuery();
-
-                List<Book> borrowedBooks = new ArrayList<>();
-                while(borrowedBooksResultSet.next()) {
-                    String title = borrowedBooksResultSet.getString("title");
-                    String author = borrowedBooksResultSet.getString("author");
-
-                    PreparedStatement bookPs = connection.prepareStatement("SELECT availableQuantity, totalQuantity FROM Books WHERE title = ? AND author = ?");
-                    bookPs.setString(1, title);
-                    bookPs.setString(2, author);
-                    ResultSet bookResultSet = bookPs.executeQuery();
-
-                    if(bookResultSet.next()) {
-                        int availableQuantity = bookResultSet.getInt("availableQuantity");
-                        int totalQuantity = bookResultSet.getInt("totalQuantity");
-                        borrowedBooks.add(new Book(title, author, availableQuantity, totalQuantity));
-                    }
-                }
-
-                // Create Reader object and borrow books
-                Reader reader = new Reader(name);
-                for (Book book : borrowedBooks) {
-                    reader.borrowBook(book);
-                }
-                readers.add(reader);
-            }
-            return readers;
-        }
-
         return null;
     }
-    
+
+    private List<Book> loadBooks() throws SQLException {
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM Books");
+        List<Book> books = new ArrayList<>();
+        while (resultSet.next()) {
+            books.add(new Book(
+                    resultSet.getString("title"),
+                    resultSet.getString("author"),
+                    resultSet.getInt("availableQuantity"),
+                    resultSet.getInt("totalQuantity")
+            ));
+        }
+        return books;
+    }
+
+    private List<Reader> loadReaders() throws SQLException {
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM Readers");
+        List<Reader> readers = new ArrayList<>();
+        while (resultSet.next()) {
+            String name = resultSet.getString("name");
+            ResultSet borrowedBooksResultSet = dealWithBorrowedBooks(name);
+            
+            List<Book> borrowedBooks = new ArrayList<>();
+            while(borrowedBooksResultSet.next()) {
+                borrowedBooks.add(new Book(
+                        borrowedBooksResultSet.getString("title"),
+                        borrowedBooksResultSet.getString("author"),
+                        0, 0
+                ));
+            }
+
+            Reader reader = new Reader(name);
+            for (Book book : borrowedBooks) {
+                reader.borrowBook(book);
+            }
+            readers.add(reader);
+        }
+        return readers;
+    }
+
+    private ResultSet dealWithBorrowedBooks(String name) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("SELECT title, author FROM BorrowedBooks WHERE name = ?");
+        ps.setString(1, name);
+        return ps.executeQuery();
+    }
+
     public void close() throws SQLException {
         if (connection != null) {
             connection.close();
